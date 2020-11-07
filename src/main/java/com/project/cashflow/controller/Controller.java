f698b2c5bfa47bcaee0c5478ba27adf4ce548c48
Allan <allanjamil_91@hotmail.com>
2020-11-08 00:28:33 +0100

    first page

--- src/main/java/com/project/cashflow/controller/Controller.java
+++ src/main/java/com/project/cashflow/controller/Controller.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
+    @GetMapping
+    public String firstPage() {
+        return "First page";
+    }
+
     @GetMapping("/hello")
     public String HelloWorld() {
         return "Hello World!";
